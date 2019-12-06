5facb1860a9a138aaaac3f73d01f7cfd89214d70
Logan Raschke <56165605+rasc1778@users.noreply.github.com>
2019-12-06 13:11:35 -0800

    update

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -13,7 +13,7 @@ public class SpawnManager : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    // Update is called once per frame.
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
